76748198e8fa333488e7e9e92f225a8bfdb17dcc
laetitiabeles <laetitiabeles9@gmail.com>
2024-04-23 17:09:03 +0200

    convert float to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Return the str rep of a float n """
+
+
+def to_str(n: float) -> str:
+    """ Return the str rep of a float
+    Args:
+        n (float): float to convert to str
+    """
+    return str(n)
