3c26ff59ad56fa0fbd132da217dfab0115f6c037
laetitiabeles <laetitiabeles9@gmail.com>
2024-04-23 17:08:42 +0200

    concatenate 2 str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Concatenate 2 str and return a concatenate str """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate 2 strings and return the result
+    Args:
+        str1 (str): First string
+         str2 (str): Second string
+    """
+    return str1 + str2
