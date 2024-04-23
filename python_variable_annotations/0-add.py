6fff1574cd73853cf08c2a40d870dc3f707913ca
laetitiabeles <laetitiabeles9@gmail.com>
2024-04-23 17:08:29 +0200

    return 2 float sum

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Returns the sum of 2 floats """
+
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of a and b
+    Args:
+        a (float): First float
+        b (float): Second float
+    """
+    return a + b
