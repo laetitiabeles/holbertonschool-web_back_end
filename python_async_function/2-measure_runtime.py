18a74a1ceb790be227b1323b961999b0e2c9fe3b
laetitiabeles <laetitiabeles9@gmail.com>
2024-04-25 11:16:39 +0200

    measure the runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+This module measures the runtime of a function using concurrent coroutines.
+'''
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''
+    Asynchronous coroutine that takes in two integer arguments and returns a float
+    '''
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return total_time / n
