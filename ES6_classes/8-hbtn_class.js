b569472c6f958c01b227cfcf36fcf3d43eedf240
laetitiabeles <laetitiabeles9@gmail.com>
2024-05-10 21:39:23 +0200

    add HolbertonClass

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,36 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
