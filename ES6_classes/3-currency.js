97d18abb3d0d2c6b01ffa76487ca05a2d1dd6deb
laetitiabeles <laetitiabeles9@gmail.com>
2024-05-10 21:31:40 +0200

    class Currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a number');
+    }
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
