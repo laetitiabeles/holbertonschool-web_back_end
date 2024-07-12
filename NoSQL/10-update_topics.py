7a797b090a0b31a906efd4b9289cbcffe7aa558c
laetitiabeles <laetitiabeles9@gmail.com>
2024-07-12 16:41:04 +0200

    fixing syntax

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,4 +4,8 @@
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+
