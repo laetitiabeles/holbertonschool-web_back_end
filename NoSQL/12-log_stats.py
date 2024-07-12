9a96327f45d244d334cf5e6f4a62a9cc9530da69
laetitiabeles <laetitiabeles9@gmail.com>
2024-07-12 16:59:06 +0200

    fixing variable names

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Log stats """
+""" provides some stats about Nginx logs stored in MongoDB """
 
 
 from pymongo import MongoClient
@@ -8,16 +8,16 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """ provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    total = logs.count_documents({})
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    collection = client.logs.nginx
+    count = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print(f"{total} logs")
+    print(f"{count} logs")
     print("Methods:")
-    for m in method:
-        count = logs.count_documents({"method": method})
+    for method in methods:
+        count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    stats = logs.count_documents({"method": "GET", "path": "/status"})
+    stats = collection.count_documents({"method": "GET", "path": "/status"})
 
     print(f"{stats} status check")
